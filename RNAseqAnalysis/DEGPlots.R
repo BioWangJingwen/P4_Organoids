af7309f3e45862bbbe365ad0745a7bc8488a963a
BioWangJingwen <95023146+BioWangJingwen@users.noreply.github.com>
2024-01-06 20:32:45 +0800

    Update DEGPlots.R

--- RNAseqAnalysis/DEGPlots.R
+++ RNAseqAnalysis/DEGPlots.R
@@ -1,1 +1,14 @@
+# P4_2RNA/P4_2_3DEG1v1/P4_2_3_2RXX_vennDEGALL
+vennALL <-
+  ggVennDiagram(lisDEG.PosALL, 
+                set_color = col.deg[names(lisDEG.PosALL)]
+  ) +
+  scale_fill_material("light-blue") +
+  scale_color_manual(values = col.deg[names(lisDEG.PosALL)]) +
+  NoLegend()
+vennALL
 
+save(lisDEG.PosALL, col.deg, file = paste0(out.dir, out.prefix, "vennDEGALL.RData"))
+pdf(paste0(out.dir, out.prefix, "vennDEGALL.pdf"), width = 9, height = 9)
+vennALL
+dev.off()
