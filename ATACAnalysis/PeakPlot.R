475ac1a58b969e52e6d59185cb1dac32cd1c4ba0
BioWangJingwen <95023146+BioWangJingwen@users.noreply.github.com>
2024-01-08 21:51:29 +0800

    Update PeakPlot.R

--- ATACAnalysis/PeakPlot.R
+++ ATACAnalysis/PeakPlot.R
@@ -11,6 +11,7 @@ myVenn(mcols(allPeaksSet_nR),
 dev.off()
 
 # P4_4ATAC/P4_4_4peakDiff/P4_4_4_5RXX_peakView/P4_4_4_5RXX_PositionPeaks
+# data: P4_4ATAC/P4_4_4peakDiff/P4_4_4_5RXX_peakView/P4_4_4_5RXX_peakAnnoPos.rds
 for (i in 1:length(peakAnno)) {
   sta <- peakAnno[[i]]@annoStat
   sta$Group <- names(peakAnno)[i]
@@ -40,4 +41,65 @@ pdf(paste0(out.dir, out.prefix, "PositionPeaks.pdf"), width = 7, height = 2.5)
 peakPlot
 dev.off()
 
-# data: P4_4ATAC/P4_4_4peakDiff/P4_4_4_5RXX_peakView/P4_4_4_5RXX_peakAnnoPos.rds
+
+# P4_4ATAC/P4_4_4peakDiff/P4_4_4_5RXX_peakView/P4_4_4_5RXX_HeatDP
+ann.row <- rowAnnotation(
+  NvsPM = dat.row$NvsPM,
+  PvsN = dat.row$PvsN,
+  MvsP = dat.row$MvsP,
+  col = list(NvsPM = c(Up = unname(col.deg["NvsPM"]), Non_DCAP = "white"),
+             PvsN = c(Up = unname(col.deg["PvsN"]), Non_DCAP = "white"),
+             MvsP = c(Up = unname(col.deg["MvsP_up"]), Down = unname(col.deg["MvsP_down"]),  Non_DCAP = "white")
+  )
+)
+
+po <- match(colnames(mat.exp), samInfoATAC$SampleID)
+
+ann.col <- 
+  columnAnnotation(
+    Source = anno_block(gp = gpar(fill = col.source)),
+    # PAM50 = samInfoATAC$PAM50[po],
+    # AIMS = samInfoATAC$AIMS[po],
+    # claudinLow = samInfoATAC$claudinLow[po],
+    `menopausal status` = samInfoATAC$`menopausal status`[po],
+    Stage = samInfoATAC$Stage[po],
+    `Molecular subtype1` = samInfoATAC$`Molecular subtype1`[po],
+    col = list(IHC = col.IHC, 
+               PAM50 = col.PAM50,
+               AIMS = col.AIMS,
+               claudinLow = col.claudinLow,
+               `menopausal status` = col.status,
+               Stage = col.stage,
+               `Molecular subtype1` = col.sub1
+    )
+  )
+
+# spl.row <- 
+#   factor(c(rep("Normal", length(gen.nor)), rep("Primary", length(gen.pri)), rep("Metastatic", length(gen.met))),
+#          levels = c("Normal", "Primary", "Metastatic"))
+spl.col <- factor(samInfoATAC$Source[po], levels = c("Normal", "Primary", "Metastatic"))
+
+set.seed(123456)
+h.DP <-
+  Heatmap(mat.exp,
+          col = circlize::colorRamp2(breaks = c(0, 5, 10), colors = col.exp5),
+          top_annotation = ann.col,
+          left_annotation = ann.row,
+          name = "Scaled_Exp",
+          # row_title_rot = 0,
+          show_row_names = F, 
+          show_row_dend = F,
+          show_column_names = F,
+          show_column_dend = F,
+          # column_title_rot = 90,
+          column_dend_reorder = F,
+          cluster_column_slices = F,
+          cluster_row_slices = F,
+          # layer_fun = layer_fun,
+          # row_split = spl.row,
+          column_split = spl.col
+  )
+
+Cairo::CairoPNG(paste0(out.dir, out.prefix, "HeatDP.png"), width = 9, height = 9, units = "in", res = 108)
+h.DP
+dev.off()
